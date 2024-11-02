10237e07b7dc816a45837ef7e3ad6091ca7dce22
Narek Tserunyan <ts.narek@gmail.com>
2024-11-02 18:15:02 +0100

    port type changed

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
+  port: Number(process.env.PG_PORT),
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
   ssl: {
